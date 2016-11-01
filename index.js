55e415686e0b3b038f12f9f7d9da347336476899
Mike Bobadilla <mike.c.bobadilla@gmail.com>
2016-10-31 22:43:26 -0700

    turn light on with button

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var five = require('johnny-five');
 var board = new five.Board();
+var connected = false;
 
+var ledFive;
+
+// Initialize Board
 board.on('ready', function(){
-	var led = new five.Led(3);
-	led.on();
+	ledFive = new five.Led(3);
+});
 
-	setTimeout(function(){ led.off();}, 6000);
+// Simple Routing
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+})
+
+// On socket connection
+io.on('connection', function(socket){
+
+	// when button pressed light turns on
+	socket.on('light', function(){
+		console.log("start light");
+
+		if(ledFive){
+			ledFive.on();
+			setTimeout(function(){ ledFive.off();}, 5000);
+		}
+	});
+})
+
+// We listen for connections on port 3000.
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
